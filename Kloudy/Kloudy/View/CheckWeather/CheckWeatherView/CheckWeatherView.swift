db5ae40416cac14475ea9842fa4d4696061dd68d
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-10-17 21:06:48 +0900

    :sparkles: CheckWeatherBasicNavigationView Layout 구현

--- Kloudy/Kloudy/View/CheckWeather/CheckWeatherView/CheckWeatherView.swift
+++ Kloudy/Kloudy/View/CheckWeather/CheckWeatherView/CheckWeatherView.swift
@@ -6,7 +6,18 @@
 //
 
 import UIKit
+import SnapKit
 
 class CheckWeatherView: UIViewController {
     
+    let checkWeatherBasicNavigationView = CheckWeatherBasicNavigationView()
+    
+    override func viewDidLoad() {
+        self.view.addSubview(checkWeatherBasicNavigationView)
+        
+        checkWeatherBasicNavigationView.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(47)
+            $0.trailing.equalToSuperview().inset(21)
+        }
+    }
 }
