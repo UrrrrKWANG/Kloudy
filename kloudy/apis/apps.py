1a0d32687fb14da1fca280aac2f3a2afedb94553
Youngjun Lee <86882798+2youngjun@users.noreply.github.com>
2022-10-17 16:41:33 +0900

    :fire: Delete Directory

--- kloudy/apis/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApisConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "apis"
--- kloudy/apis/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Weather
-
-class WeatherSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Weather
-        fields = "__all__"
