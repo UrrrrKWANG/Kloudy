103e0a2f71342c12a6bb9fdbdcc70531bf08ce73
Geunil Park <77421835+Valentino1994@users.noreply.github.com>
2022-10-19 00:51:46 +0900

    Feat[#2] : 현재 날씨 값을 받아오는 함수 만듦

--- Server/apis/api_calls/weatherAPI.py
+++ Server/apis/api_calls/weatherAPI.py
@@ -15,10 +15,35 @@ def weatherAPI(day, time, x_coordinate, y_coordinate):
     
     headers = {'Content-Type': 'application/json', 'charset': 'UTF-8', 'Accept': '*/*'}
 
-    url = f'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey={METEOROGICAL_KEY}&numOfRows=19&pageNo=1&dataType=JSON&base_date={day}&base_time={time}&nx={x_coordinate}&ny={y_coordinate}'
+    current_weather = getCurrentWeather(headers, METEOROGICAL_KEY, day, time, x_coordinate, y_coordinate)
 
+    print(f"This is SKY_STATUS : {sky_status}")    
+
+    return Weather(name = "test", visibility = "test")
+
+def getCurrentWeather(headers, key, day, time, x_coordinate, y_coordinate):
+    url = f'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtFcst?serviceKey={key}&numOfRows=19&pageNo=1&dataType=JSON&base_date={day}&base_time={time}&nx={x_coordinate}&ny={y_coordinate}'
+    
     response = requests.get(url, headers=headers)
     jsonObject = json.loads(response.text)
+    # 만약 currentWeather 3 or 4라면 다른 요청을 보내야함 맑으면 보낼 필요 없음
+    nowStatus = jsonObject.get('response').get('body').get('items').get('item')[0].get('fcstValue')
+    if nowStatus == "0":
+        return "0"
+    else:
+        rainURL = f'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey={key}&numOfRows=1&pageNo=1&dataType=JSON&base_date={day}&base_time={time}&nx={x_coordinate}&ny={y_coordinate}'
+        response = requests.get(rainURL, headers=headers)
+        jsonObject = json.loads(response.text)
+        nowRain = jsonObject.get('response').get('body').get('items').get('item')[0].get('obsrValue')
+        if nowRain == "0":
+            return nowStatus
+        # 3번은 nowStatus랑 겹칠 수 있음
+        else:
+            return "5" if nowRain == "3" else nowRain
+    return
 
-    print(jsonObject.get('response'))
-    return Weather(name = "test", visibility = "test")
+def getCurrentTemperature(headers, key, day, time, x_coordinate, y_coordinate):
+    return
+
+def getMaxMinTemperature(headers, key, day, time, x_coordinate, y_coordinate):
+    return
