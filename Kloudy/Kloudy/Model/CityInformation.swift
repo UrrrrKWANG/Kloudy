88649486f1bfdf0c4759d88e270adaed70e33d31
Geunil Park <77421835+Valentino1994@users.noreply.github.com>
2022-10-18 23:20:51 +0900

    Feat[#2] : 도시의 x값과 y값을 받아오는 로직을 만들었습니다.

--- Kloudy/Kloudy/Model/CityInformation.swift
+++ Kloudy/Kloudy/Model/CityInformation.swift
@@ -13,9 +13,6 @@ struct CityInformation {
     let y: Int
 }
 
-// 군들이 빠져있음 -> 나중에 엑셀파일에서 긁어오겠음
-let koreaCitySamples: [CityInformation] = [
-    CityInformation(name: "서울시", x: 60, y: 127),
-    CityInformation(name: "인천시", x: 55, y: 124),
-    CityInformation(name: "수원시", x: 60, y: 121)
-]
+
+
+
--- Kloudy/Kloudy/Model/FetchWeatherInformation.swift
+++ Kloudy/Kloudy/Model/FetchWeatherInformation.swift
@@ -51,7 +51,15 @@ class FetchWeatherInformation {
     }
     
     func findCityCoordinate(city: String) -> [String] {
+        // 군들이 빠져있음 -> 나중에 엑셀파일에서 긁어오겠음
+        let koreaCitySamples: [CityInformation] = [
+            CityInformation(name: "서울시", x: 60, y: 127),
+            CityInformation(name: "인천시", x: 55, y: 124),
+            CityInformation(name: "수원시", x: 60, y: 121)
+        ]
         
-        return []
+        let nowCity = koreaCitySamples.filter{ $0.name == city }[0]
+        
+        return [String(nowCity.x), String(nowCity.y)]
     }
 }
