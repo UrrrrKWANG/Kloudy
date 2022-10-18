c3549f888eee43828eae37f70c8eb6c4d32a8bbe
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-10-18 20:37:53 +0900

    :bento: Color Asset 추가 및 Kloudy+Color 파일 최종 구현

--- Kloudy/Kloudy/Extension/Kloudy+Font.swift
+++ Kloudy/Kloudy/Extension/Kloudy+Font.swift
@@ -7,76 +7,26 @@
 
 import UIKit
 
-enum KFont {
-    case lexendExtraLarge
-    case lexendLarge
-    case lexendMedium
-    case lexendSmall
-    case lexendMini
-    
-    case appleSDNeoBoldLarge
-    case appleSDNeoBoldMedium
-    case appleSDNeoBoldSmall
-    
-    case appleSDNeoSemiBoldLarge
-    case appleSDNeoSemiBoldMedium
-    case appleSDNeoSemiBoldSmall
-    case appleSDNeoSemiBoldMini
-    
-    case appleSDNeoMediumLarge
-    case appleSDNeoMediumMedium
-    case appleSDNeoMediumSmall
-    
-    case appleSDNeoRegularLarge
-    case appleSDNeoRegularMedium
-    case appleSDNeoRegularSmall
-}
-
 extension UIFont {
-    static func customFont(_ name: KFont) -> UIFont {
-        switch name {
-        case .lexendExtraLarge:
-            return UIFont(name: "lexendExtraLarge", size: 100) ?? UIFont()
-        case .lexendLarge:
-            return UIFont(name: "Lexend-Regular", size: 34) ?? UIFont()
-        case .lexendMedium:
-            return UIFont(name: "Lexend-Regular", size: 20) ?? UIFont()
-        case .lexendSmall:
-            return UIFont(name: "Lexend-Regular", size: 15) ?? UIFont()
-        case .lexendMini:
-            return UIFont(name: "Lexend-Regular", size: 14) ?? UIFont()
-            
-        case .appleSDNeoBoldLarge:
-            return UIFont(name: "AppleSDGothicNeo-Bold", size: 24) ?? UIFont()
-        case .appleSDNeoBoldMedium:
-            return UIFont(name: "AppleSDGothicNeo-Bold", size: 20) ?? UIFont()
-        case .appleSDNeoBoldSmall:
-            return UIFont(name: "AppleSDGothicNeo-Bold", size: 18) ?? UIFont()
-            
-            
-        case .appleSDNeoSemiBoldLarge:
-            return UIFont(name: "AppleSDGothicNeo-SemiBold", size: 18) ?? UIFont()
-        case .appleSDNeoSemiBoldMedium:
-            return UIFont(name: "AppleSDGothicNeo-SemiBold", size: 17) ?? UIFont()
-        case .appleSDNeoSemiBoldSmall:
-            return UIFont(name: "AppleSDGothicNeo-SemiBold", size: 14) ?? UIFont()
-        case .appleSDNeoSemiBoldMini:
-            return UIFont(name: "AppleSDGothicNeo-SemiBold", size: 12) ?? UIFont()
-            
-            
-        case .appleSDNeoMediumLarge:
-            return   UIFont(name: "AppleSDGothicNeo-Medium", size: 20) ?? UIFont()
-        case .appleSDNeoMediumMedium:
-            return   UIFont(name: "AppleSDGothicNeo-Medium", size: 15) ?? UIFont()
-        case .appleSDNeoMediumSmall:
-            return   UIFont(name: "AppleSDGothicNeo-Medium", size: 14) ?? UIFont()
-            
-        case .appleSDNeoRegularLarge:
-            return   UIFont(name: "AppleSDGothicNeo-Regular", size: 18) ?? UIFont()
-        case .appleSDNeoRegularMedium:
-            return   UIFont(name: "AppleSDGothicNeo-Regular", size: 17) ?? UIFont()
-        case .appleSDNeoRegularSmall:
-            return   UIFont(name: "AppleSDGothicNeo-Regular", size: 15) ?? UIFont()
-        }
+    // 사용 방법 : UIColor.KColor.lexendExtraLarge
+    enum KFont {
+        static var lexendExtraLarge: UIFont { UIFont(name: "Lexend-Regular", size: 100)! }
+        static var lexendLarge: UIFont { UIFont(name: "Lexend-Regular", size: 34)! }
+        static var lexendMedium: UIFont { UIFont(name: "Lexend-Regular", size: 20)! }
+        static var lexendSmall: UIFont { UIFont(name: "Lexend-Regular", size: 15)! }
+        static var lexendMini: UIFont { UIFont(name: "Lexend-Regular", size: 14)! }
+        static var appleSDNeoBoldLarge: UIFont { UIFont(name: "AppleSDGothicNeo-Bold", size: 24)! }
+        static var appleSDNeoBoldMedium: UIFont { UIFont(name: "AppleSDGothicNeo-Bold", size: 20)! }
+        static var appleSDNeoBoldSmall: UIFont { UIFont(name: "AppleSDGothicNeo-Bold", size: 18)! }
+        static var appleSDNeoSemiBoldLarge: UIFont { UIFont(name: "AppleSDGothicNeo-SemiBold", size: 18)! }
+        static var appleSDNeoSemiBoldMedium: UIFont { UIFont(name: "AppleSDGothicNeo-SemiBold", size: 17)! }
+        static var appleSDNeoSemiBoldSmall: UIFont { UIFont(name: "AppleSDGothicNeo-SemiBold", size: 14)! }
+        static var appleSDNeoSemiBoldMini: UIFont { UIFont(name: "AppleSDGothicNeo-SemiBold", size: 12)! }
+        static var appleSDNeoMediumLarge: UIFont { UIFont(name: "AppleSDGothicNeo-Medium", size: 20)! }
+        static var appleSDNeoMediumMedium: UIFont { UIFont(name: "AppleSDGothicNeo-Medium", size: 15)! }
+        static var appleSDNeoMediumSmall: UIFont { UIFont(name: "AppleSDGothicNeo-Medium", size: 14)! }
+        static var appleSDNeoRegularLarge: UIFont { UIFont(name: "AppleSDGothicNeo-Regular", size: 18)! }
+        static var appleSDNeoRegularMedium: UIFont { UIFont(name: "AppleSDGothicNeo-Regular", size: 17)! }
+        static var appleSDNeoRegularSmall: UIFont { UIFont(name: "AppleSDGothicNeo-Regular", size: 15)! }
     }
 }
