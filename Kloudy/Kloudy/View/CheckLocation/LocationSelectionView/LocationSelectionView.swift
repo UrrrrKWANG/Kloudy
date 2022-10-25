3a2bd43d1d5f9aa1d722a553349bc2a87f057c9b
sseulkii <operry126@gmail.com>
2022-10-25 22:54:47 +0900

    :sparkles: #9 collectionview 수정

--- Kloudy/Kloudy/View/CheckLocation/LocationSelectionView/LocationSelectionView.swift
+++ Kloudy/Kloudy/View/CheckLocation/LocationSelectionView/LocationSelectionView.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import SnapKit
 
 let cellID = "Cell"
 
@@ -17,18 +18,50 @@ class LocationSelectionView: UIViewController {
         
         return cv
     }()
+
+    let searchButton: UIButton = {
+        let searchBar = UIButton()
+        searchBar.setTitle("지역을 검색해 보세요", for: .normal)
+        searchBar.setImage(UIImage(systemName: "magnifyingglass"), for: .normal)
+        searchBar.imageView?.contentMode = .scaleAspectFill
+        searchBar.backgroundColor = UIColor.KColor.gray02
+        searchBar.contentHorizontalAlignment = .center
+        searchBar.semanticContentAttribute = .forceRightToLeft
+        searchBar.titleEdgeInsets = .init(top:0, left: -10, bottom: 0, right: 0)
+        searchBar.imageEdgeInsets = .init(top: 0, left: 0, bottom: 0, right: -10)
+        
+        return searchBar
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .black
         
         collectionView.dataSource = self
         collectionView.delegate = self
         
         view.addSubview(collectionView)
+        view.addSubview(searchButton)
         
         collectionView.translatesAutoresizingMaskIntoConstraints = false
         
-        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        collectionView.backgroundColor = .black
+
+        searchButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        searchButton.layer.cornerRadius = 10
+        
+        searchButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+//        searchButton.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        searchButton.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        searchButton.bottomAnchor.constraint(equalTo: collectionView.topAnchor).isActive = true
+        searchButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        
+        searchButton.widthAnchor.constraint(equalToConstant: view.frame.width - 30).isActive = true
+        searchButton.heightAnchor.constraint(equalToConstant: 45).isActive = true
+                
+        collectionView.topAnchor.constraint(equalTo: searchButton.bottomAnchor).isActive = true
         collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
         collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
@@ -48,9 +81,9 @@ extension LocationSelectionView: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! LocationSelectionCollectionViewCell
         
-        cell.layer.cornerRadius = 10
+        cell.layer.cornerRadius = 15
         
-        cell.backgroundColor = .lightGray
+        cell.backgroundColor = UIColor.KColor.gray02
         
         return cell
     }
@@ -70,7 +103,11 @@ extension LocationSelectionView: UICollectionViewDelegateFlowLayout {
         return 10
     }
     
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 15
+    }
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+        return UIEdgeInsets(top: 30, left: 10, bottom: 10, right: 10)
     }
 }
