dc3364d8ad9678f8818aa4ac27db334b614bbf7d
dkdo1406 <54826394+dkdo1406@users.noreply.github.com>
2022-10-25 16:01:43 +0900

    :fire: 테스트코드 삭제

--- Kloudy/Kloudy/View/CheckWeather/CheckLocationWeatherView/CheckLocationWeatherView.swift
+++ Kloudy/Kloudy/View/CheckWeather/CheckLocationWeatherView/CheckLocationWeatherView.swift
@@ -9,13 +9,12 @@ import UIKit
 import SnapKit
 
 class CheckLocationWeatherView: UIView {
-    
+//TODO: 임시로 이미지 파일 넣었습니다.
     let nameStackView = UIStackView()
-    
     let locationImage: UIImageView = {
         let aLocationIcon = UIImageView()
         //        aLocationIcon.image = UIImage(named: "camera.macro")
-        aLocationIcon.image = UIImage(systemName: "camera.macro")
+        aLocationIcon.image = UIImage(systemName: "location.circle.fill")
         aLocationIcon.snp.makeConstraints {
             $0.size.equalTo(20)
         }
@@ -24,9 +23,10 @@ class CheckLocationWeatherView: UIView {
     let locationLabel = UILabel()
     let weatherImage: UIImageView = {
         let aweatherImage = UIImageView()
-        aweatherImage.image = UIImage(systemName: "camera.macro")
+        aweatherImage.image = UIImage(systemName: "cloud.fill")
         aweatherImage.snp.makeConstraints {
-            $0.size.equalTo(20)
+            $0.width.equalTo(153)
+            $0.height.equalTo(120)
         }
         return aweatherImage
     }()
@@ -37,27 +37,19 @@ class CheckLocationWeatherView: UIView {
     let maxTemperatureImage: UIImageView = {
         let aMaxTemperatureImage = UIImageView()
         aMaxTemperatureImage.image = UIImage(systemName: "arrow.up")
-        aMaxTemperatureImage.snp.makeConstraints {
-            $0.size.equalTo(20)
-        }
         return aMaxTemperatureImage
     }()
-    
     let minStackView = UIStackView()
     let minTemperatureLabel = UILabel()
     let minTemperatureImage: UIImageView = {
         let aMinTemperatureImage = UIImageView()
         aMinTemperatureImage.image = UIImage(systemName: "arrow.down")
-        aMinTemperatureImage.snp.makeConstraints {
-            $0.size.equalTo(20)
-        }
         return aMinTemperatureImage
     }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
-    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
@@ -65,12 +57,11 @@ class CheckLocationWeatherView: UIView {
     override func layoutSubviews() {
         super.layoutSubviews()
         setUp()
-        
     }
     
     func setUp() {
-        // 서브뷰 소환
         self.addSubview(nameStackView)
+        self.addSubview(weatherImage)
         self.addSubview(temperatureLabel)
         self.addSubview(minmaxStackView)
         self.addSubview(maxStackView)
@@ -78,6 +69,11 @@ class CheckLocationWeatherView: UIView {
 
         configureLocationLabel()
         configuretemperatureLabel()
+        //TODO: 현재 날씨 이미지 위치가 안잡혀서 임시로 넣었습니다.
+        weatherImage.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(50)
+            $0.leading.equalToSuperview().inset(17)
+        }
         
         [maxStackView, minStackView].forEach {
             minmaxStackView.addArrangedSubview($0)
@@ -88,25 +84,17 @@ class CheckLocationWeatherView: UIView {
             $0.trailing.equalToSuperview().inset(21)
             $0.bottom.equalToSuperview().inset(26)
         }
+        minmaxStackView.backgroundColor = .black
+        maxStackView.backgroundColor = .red
+        minStackView.backgroundColor = .blue
         
         maxStackView.clipsToBounds = true
         maxStackView.layer.cornerRadius = 20
-        
         minStackView.clipsToBounds = true
         minStackView.layer.cornerRadius = 20
         
-        maxStackView.snp.makeConstraints {
-            $0.height.equalTo(32)
-            $0.width.equalTo(92)
-        }
-        minStackView.snp.makeConstraints {
-            $0.height.equalTo(32)
-            $0.width.equalTo(92)
-        }
-        
-        // 스택뷰 초기 설정
         configureStackView()
-        // 스택뷰이기 때문에 차례대로 소환
+        
         [locationImage, locationLabel].forEach {
             nameStackView.addArrangedSubview($0)
         }
@@ -117,17 +105,16 @@ class CheckLocationWeatherView: UIView {
             minStackView.addArrangedSubview($0)
         }
         
-        // 스택뷰 콘스트레인트 설정
         nameStackView.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalToSuperview().inset(20)
-//            $0.height.equalTo(32)
-//            $0.width.equalTo(100)
             $0.width.equalTo((locationLabel.text!.count+2) * 15)
-                        
         }
         
-        // 스택뷰 안에 콘스트레인트 설정
+        maxStackView.isLayoutMarginsRelativeArrangement = true
+        maxStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 10)
+        minStackView.isLayoutMarginsRelativeArrangement = true
+        minStackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: 0, leading: 12, bottom: 0, trailing: 10)
         
         configureMaxTemperatureLabel()
         configureMinTemperatureLabel()
@@ -153,31 +140,19 @@ class CheckLocationWeatherView: UIView {
         minStackView.distribution = .fillProportionally
         minStackView.spacing = 0
     }
-    
-    
-    private func configureMinTemperatureImage() {
-        minTemperatureImage.snp.makeConstraints {
-//            $0.centerY.equalTo(minStackView)
-            $0.trailing.equalTo(minStackView.snp.trailing).offset(25)
-            $0.size.equalTo(20)
-        }
-    }
-    
+
+//TODO: 임시로 위치, 온도 넣었습니다.
     private func configureLocationLabel() {
         locationLabel.text = "서울"
-        locationLabel.font = locationLabel.font.withSize(18)
+        locationLabel.font = UIFont.KFont.appleSDNeoSemiBoldLarge
+        locationLabel.textColor = UIColor.white
         locationLabel.textAlignment = .right
-        //        locationLabel.backgroundColor = .black
-//        locationLabel.snp.makeConstraints {
-//            $0.centerX.equalTo(nameStackView)
-//            $0.trailing.equalTo(nameStackView.snp.trailing).offset(0)
-//        }
     }
     
     private func configuretemperatureLabel() {
         temperatureLabel.text = "19°"
-        temperatureLabel.font = temperatureLabel.font.withSize(100)
-//        temperatureLabel.backgroundColor = .black
+        temperatureLabel.font = UIFont.KFont.lexendExtraLarge
+        temperatureLabel.textColor = UIColor.white
         temperatureLabel.snp.makeConstraints {
             $0.centerX.equalToSuperview()
             $0.top.equalTo(nameStackView.snp.bottom).offset(20)
@@ -188,27 +163,18 @@ class CheckLocationWeatherView: UIView {
     
     private func configureMaxTemperatureLabel() {
         maxTemperatureLabel.text = "최고  19°"
-        maxTemperatureLabel.font = maxTemperatureLabel.font.withSize(12)
-        maxTemperatureLabel.snp.makeConstraints {
-            $0.centerY.equalTo(maxStackView)
-            $0.leading.equalTo(maxStackView.snp.leading).inset(12)
-//            $0.width.equalTo(50)
-            $0.trailing.equalTo(maxTemperatureImage.snp.leading).offset(0)
-        }
+        maxTemperatureLabel.font = UIFont.KFont.appleSDNeoMediumSmall
+        maxTemperatureLabel.textColor = UIColor.white
         maxTemperatureImage.snp.makeConstraints {
-            $0.centerY.equalTo(maxStackView)
-            $0.trailing.equalTo(maxStackView.snp.trailing).offset(12)
-//            $0.width.equalTo(50)
-//            $0.trailing.equalTo(maxTemperatureImage.snp.leading).offset(0)
+            $0.size.equalTo(20)
         }
     }
     private func configureMinTemperatureLabel() {
         minTemperatureLabel.text = "최저  12°"
-        minTemperatureLabel.font = maxTemperatureLabel.font.withSize(12)
-        minTemperatureLabel.snp.makeConstraints {
-            $0.centerY.equalTo(minStackView)
-            $0.leading.equalTo(minStackView.snp.leading).offset(12)
+        minTemperatureLabel.font = UIFont.KFont.appleSDNeoMediumSmall
+        minTemperatureLabel.textColor = UIColor.white
+        minTemperatureImage.snp.makeConstraints {
+            $0.size.equalTo(20)
         }
     }
 }
-
