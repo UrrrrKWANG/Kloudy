bc5b2d20c55b35080b416caab35d78a989741b93
sseulkii <operry126@gmail.com>
2022-10-25 17:14:54 +0900

    :sparkles: #9 collectionview 추가

--- Kloudy/Kloudy/View/CheckLocation/LocationSelectionView/LocationSelectionView.swift
+++ Kloudy/Kloudy/View/CheckLocation/LocationSelectionView/LocationSelectionView.swift
@@ -5,4 +5,72 @@
 //  Created by 이영준 on 2022/10/17.
 //
 
-import Foundation
+import UIKit
+
+let cellID = "Cell"
+
+class LocationSelectionView: UIViewController {
+
+    let collectionView: UICollectionView = {
+        let flowlayout = UICollectionViewFlowLayout()
+        let cv = UICollectionView(frame: .zero, collectionViewLayout: flowlayout)
+        
+        return cv
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        
+        view.addSubview(collectionView)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        collectionView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        collectionView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+                
+        collectionView.register(LocationSelectionCollectionViewCell.self, forCellWithReuseIdentifier: cellID)
+    }
+    
+    
+}
+
+extension LocationSelectionView: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath) as! LocationSelectionCollectionViewCell
+        
+        cell.layer.cornerRadius = 10
+        
+        cell.backgroundColor = .lightGray
+        
+        return cell
+    }
+}
+
+extension LocationSelectionView: UICollectionViewDelegate {
+    
+}
+
+extension LocationSelectionView: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: view.frame.width - 30, height: view.frame.width / 4)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 5, left: 5, bottom: 5, right: 5)
+    }
+}
