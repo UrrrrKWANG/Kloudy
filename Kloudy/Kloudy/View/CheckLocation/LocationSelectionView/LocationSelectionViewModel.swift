7003b59ede0d745a43a7b8bb5f788b4a6330e153
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-10-27 03:57:50 +0900

    :sparkles: Location 추가 시 CoreData 에 저장되는 로직 구현

--- Kloudy/Kloudy/View/CheckLocation/LocationSelectionView/LocationSelectionViewModel.swift
+++ Kloudy/Kloudy/View/CheckLocation/LocationSelectionView/LocationSelectionViewModel.swift
@@ -5,4 +5,26 @@
 //  Created by 이영준 on 2022/10/17.
 //
 
-import Foundation
+import UIKit
+import CoreData
+
+class LocationSelectionViewModel {
+    var coreDataStack = CoreDataStack(modelName: "Location")
+    
+    func saveLocation(city: String, latitude: String, longtitude: String, sequence: Int) {
+        let request: NSFetchRequest<Location> = Location.fetchRequest()
+        do {
+            let locations = try coreDataStack.managedContext.fetch(request)
+            let countLocations = locations.count
+            
+            let location = Location(context: coreDataStack.managedContext)
+            location.city = city
+            location.latitude = Double(latitude) ?? 0
+            location.longitude = Double(longtitude) ?? 0
+            location.sequence = Int16(countLocations)
+            coreDataStack.saveContext()
+        } catch {
+            print("-----fetchLocationsError-----")
+        }
+    }
+}
