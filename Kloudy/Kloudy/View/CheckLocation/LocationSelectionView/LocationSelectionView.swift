4491357959075722da6b77523d401816fb3e191b
sseulkii <operry126@gmail.com>
2022-10-26 23:24:00 +0900

    :bulb: #9 코멘트 추가하였습니다.

--- Kloudy/Kloudy/View/CheckLocation/LocationSelectionView/LocationSelectionView.swift
+++ Kloudy/Kloudy/View/CheckLocation/LocationSelectionView/LocationSelectionView.swift
@@ -19,6 +19,7 @@ class LocationSelectionView: UIViewController {
         return cv
     }()
 
+    // 롱탭의 시작점과 끝점이 같을 경우에만 롱탭제스쳐를 활성화하기 위하여 설정한 변수
     var currentLongPressedCell: LocationSelectionCollectionViewCell?
     
     override func viewDidLoad() {
@@ -35,18 +36,22 @@ class LocationSelectionView: UIViewController {
         
         collectionView.backgroundColor = .black
 
+        // 스냅킷 사용하여 오토레이아웃 간략화하였습니다.
         collectionView.snp.makeConstraints {
             $0.top.left.right.bottom.equalToSuperview()
         }
                 
         collectionView.register(LocationSelectionCollectionViewCell.self, forCellWithReuseIdentifier: LocationSelectionCollectionViewCell.cellID)
         
+        // 롱탭제스쳐 활성화 함수
         setUpLongGestureRecognizerOnCollection()
     }
     
     
 }
 
+
+// MARK: UICollectionViewDataSource 익스텐션
 extension LocationSelectionView: UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -64,10 +69,14 @@ extension LocationSelectionView: UICollectionViewDataSource {
     }
 }
 
+
+// MARK: UICollectionViewDelegate 익스텐션
 extension LocationSelectionView: UICollectionViewDelegate {
     
 }
 
+
+// MARK: UICollectionViewDelegateFlowLayout 익스텐션
 extension LocationSelectionView: UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -87,12 +96,16 @@ extension LocationSelectionView: UICollectionViewDelegateFlowLayout {
     }
 }
 
+
+// MARK: 롱탭제스쳐 관련 코드
 // http://yoonbumtae.com/?p=4418
 
 extension LocationSelectionView: UIGestureRecognizerDelegate {
 
+    // 롱탭제스쳐 recognizer 함수
     private func setUpLongGestureRecognizerOnCollection() {
 
+        // 롱탭제스쳐 기본 설정 코드
         let longPressedGesture = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(gestureRecognizer:)))
         longPressedGesture.minimumPressDuration = 0.5
         longPressedGesture.delegate = self
@@ -104,16 +117,17 @@ extension LocationSelectionView: UIGestureRecognizerDelegate {
 
         let location = gestureRecognizer.location(in: collectionView)
 
+        // 롱탭제스쳐 시작할 때의 코드
         if gestureRecognizer.state == .began {
             if let indexPath = collectionView.indexPathForItem(at: location) {
                 UIView.animate(withDuration: 0.2) { [self] in
                     if let cell = self.collectionView.cellForItem(at: indexPath) as? LocationSelectionCollectionViewCell {
                         self.currentLongPressedCell = cell
-                        cell.transform = .init(scaleX: 0.95, y: 0.95)
+                        cell.transform = .init(scaleX: 0.95, y: 0.95) // 임시코드입니다.
                     }
                 }
             }
-        } else if gestureRecognizer.state == .ended {
+        } else if gestureRecognizer.state == .ended { // 롱탭제스쳐 끝날 때의 코드
             if let indexPath = collectionView.indexPathForItem(at: location) {
                 UIView.animate(withDuration: 0.2) { [self] in
                     if let cell = self.currentLongPressedCell {
@@ -121,6 +135,7 @@ extension LocationSelectionView: UIGestureRecognizerDelegate {
 
                         if cell == self.collectionView.cellForItem(at: indexPath) as? LocationSelectionCollectionViewCell {
                             // .began에서 저장했던 cell과 현재 위치에 있는 셀이 같다면 동작을 실행하고, 아니라면 아무것도 하지 않습니다.
+                            // 코드 추가 예정
                         }
                     }
                 }
