95bfb1e15cfafd68f54d90cade42af57c06078ff
dkdo1406 <54826394+dkdo1406@users.noreply.github.com>
2022-11-08 14:22:24 +0900

    :lipstick: #135 레이아웃 수정, info에 있던 main삭제

--- Kloudy/Kloudy/View/CheckWeather/CheckWeatherView/CheckWeatherView.swift
+++ Kloudy/Kloudy/View/CheckWeather/CheckWeatherView/CheckWeatherView.swift
@@ -2,133 +2,180 @@
 //  CheckWeatherView.swift
 //  Kloudy
 //
-//  Created by 이영준 on 2022/10/17.
+//  Created by Hyeongjung on 2022/11/07.
 //
 
 import UIKit
-import SwiftUI
-import Combine
+import SnapKit
 
-protocol SendFirstSequenceLocationDelegate: AnyObject {
-    func sendFirstSequenceLocation(_ location : Location)
-}
+// https://www.linkedin.com/pulse/using-ios-pageviewcontroller-without-storyboards-paul-tangen/
+// https://ios-development.tistory.com/623
 
 class CheckWeatherView: UIViewController {
+    
     let checkWeatherBasicNavigationView = CheckWeatherBasicNavigationView()
-    let checkWeatherCellLabelView = CheckWeatherCellLabelView()  //생활지수 라벨
-    let addLivingIndexCellView = AddLivingIndexCellView()
-    let CheckWeatherPageView = TmpCheckWeatherPageView()
     
-    let cityInformationModel = FetchWeatherInformation()
-    let viewModel = LocationSelectionViewModel()
-    var locations = [Location]()
-    var firstSequenceLocation = Location()
+    let pageControl = UIPageControl()
+    let initialPage = 0
     
-    // csv 파일 데이터
-    var cityInformation = [CityInformation]()
-    var firstSequenceProvince = ""
-    var firstSequenceCity = ""
+    lazy var pageViewController: UIPageViewController = {
+        let vc = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        
+        return vc
+    }()
+    lazy var test = ["pohang", "daegu", "jeju"]
     
-    @ObservedObject var fetchedWeatherInfo = FetchWeatherInformation()
-    var cancelBag = Set<AnyCancellable>()
-    var weatherData: Weather = Weather(today: "", main: [], weatherIndex: [])
-    let checkLocationWeatherView = CheckLocationWeatherView()
-    weak var delegate: SendFirstSequenceLocationDelegate?
+    var dataViewControllers = [UIViewController]()
     
-    //MARK: View LifeCycle Function
+    func loadWeatherView() {
+        test.forEach { city in
+            // 여기서 Controller를 그린다.
+            // view를 그릴 때 viewModel을 가져와 사용한다.
+            // viewmodel에 init을 줘서 코어데이터에 있는 도시를 가져와서 사용하면 될 듯
+            lazy var num: UIViewController = {
+                let vc = UIViewController()
+                vc.view.backgroundColor = .brown
+                let test = UITextView()
+                let locationView = UIView()
+                //    let weatherIndexView = WeatherIndexView()
+                //    let detailWeatherView = DetailWeatherView()
+                let weatherIndexView = UIView()
+                let detailWeatherView = UIView()
+                vc.view.addSubview(test)
+                test.text = city
+                test.snp.makeConstraints {
+                    $0.top.trailing.equalToSuperview().inset(30)
+                    $0.size.equalTo(100)
+                }
+                vc.view.addSubview(locationView)
+                vc.view.addSubview(weatherIndexView)
+                vc.view.addSubview(detailWeatherView)
+                
+                locationView.backgroundColor = .red
+                locationView.snp.makeConstraints {
+                    $0.top.equalToSuperview().inset(28)
+                    $0.leading.equalToSuperview().inset(20)
+                    $0.width.equalTo(203)
+                    $0.height.equalTo(150)
+                }
+                weatherIndexView.backgroundColor = .blue
+                weatherIndexView.snp.makeConstraints {
+                    $0.top.equalTo(locationView.snp.bottom).offset(24)
+                    $0.leading.equalToSuperview().inset(20)
+                    $0.width.equalTo(350)
+                    $0.height.equalTo(360)
+                }
+                detailWeatherView.backgroundColor = .green
+                detailWeatherView.snp.makeConstraints {
+                    $0.top.equalTo(weatherIndexView.snp.bottom).offset(20)
+                    $0.leading.equalToSuperview().inset(20)
+                    $0.width.equalTo(350)
+                    $0.height.equalTo(43)
+                }
+                return vc
+            }()
+            dataViewControllers.append(num)
+            
+        }
+    }
+    
     override func viewDidLoad() {
-        if UserDefaults.standard.bool(forKey: "launchedBefore") == false {
-            viewModel.saveLocation(city: "1111000000", latitude: 37, longtitude: 126, sequence: 0)
-            UserDefaults.standard.set(true, forKey: "launchedBefore")
-        }
-        self.cityInformation = cityInformationModel.loadCityListFromCSV()
+        super.viewDidLoad()
+        loadWeatherView()
+        view.backgroundColor = .white
+        navigationController?.navigationBar.isHidden = true
+        view.addSubview(checkWeatherBasicNavigationView)
+        configureCheckWeatherNavigationView()
         
-        self.delegate = self.addLivingIndexCellView
-        self.navigationController?.navigationBar.isHidden = true
-        self.view.backgroundColor = UIColor.KColor.backgroundBlack
-
-        self.view.addSubview(checkWeatherBasicNavigationView)
-        self.configureCheckWeatherBasicNavigationView()
-        // 코드 구현을 위해 BasicNavigationView 의 경우 isHidden 처리
-        self.checkWeatherBasicNavigationView.isHidden = false
+        addChild(pageViewController)
+        view.addSubview(pageViewController.view)
+        pageViewController.dataSource = self
+        pageViewController.delegate = self
+        configurePageViewController()
         
-        self.view.addSubview(checkLocationWeatherView)
-        checkLocationWeatherView.snp.makeConstraints {
-            $0.top.equalTo(checkWeatherBasicNavigationView.snp.bottom).offset(16)
-            $0.width.equalTo(UIScreen.main.bounds.width)
-            $0.height.equalTo(181)
+        if let firstVC = dataViewControllers.first {
+            pageViewController.setViewControllers([firstVC], direction: .forward, animated: true, completion: nil)
         }
         
-        self.view.addSubview(checkWeatherCellLabelView)
-        checkWeatherCellLabelView.snp.makeConstraints {
-            $0.top.equalTo(checkLocationWeatherView.snp.bottom).offset(25)
-            $0.leading.trailing.equalToSuperview().inset(21)
-            $0.height.equalTo(180)
-        }
-        checkWeatherCellLabelView.addSubview(CheckWeatherPageView)
-        CheckWeatherPageView.snp.makeConstraints {
-            $0.height.width.equalToSuperview()
-//            $0.height.equalTo(UIScreen.main.bounds.height/5)
-//            $0.width.equalTo(UIScreen.main.bounds.width)
-            $0.top.equalToSuperview().offset(10)
+        //pageControl 부분
+        self.pageControl.frame = CGRect()
+        self.pageControl.currentPageIndicatorTintColor = UIColor.black
+        self.pageControl.pageIndicatorTintColor = UIColor.lightGray
+        self.pageControl.numberOfPages = self.dataViewControllers.count
+        self.pageControl.currentPage = initialPage
+        self.view.addSubview(self.pageControl)
+        
+        self.pageControl.snp.makeConstraints {
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).inset(6)
+            $0.centerX.equalToSuperview()
         }
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        // sequence 가 가장 낮은 Location 을 반환
-        self.locations = viewModel.fetchLocations()
-        self.firstSequenceLocation = self.locations[0]
-        
-        self.cityInformation.forEach { info in
-            if info.code == firstSequenceLocation.city {
-                self.firstSequenceProvince = info.province
-                self.firstSequenceCity = info.city
-            }
-        }
-        
-        // 기상 데이터 요청
-        self.fetchedWeatherInfo.$result
-            .receive(on: DispatchQueue.main)
-            .sink(receiveValue: { [weak self] _ in
-                // add action
-                self!.weatherData = (self?.fetchedWeatherInfo.result)!
-                self!.setLocationWeatherView()
-            })
-            .store(in: &self.cancelBag)
-        fetchedWeatherInfo.startLoad(province: self.firstSequenceProvince, city: self.firstSequenceCity)
-    }
-    
-    //MARK: Style Function
-    private func configureCheckWeatherBasicNavigationView() {
+    func configureCheckWeatherNavigationView() {
+        checkWeatherBasicNavigationView.backgroundColor = .black
         checkWeatherBasicNavigationView.snp.makeConstraints {
-            $0.top.equalToSuperview().inset(63)
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).inset(16)
             $0.trailing.equalToSuperview().inset(21)
             $0.width.equalTo(103)
             $0.height.equalTo(20)
+            
+            //            checkWeatherBasicNavigationView.locationButton.addTarget(self, action: #selector(tapLocationButton), for: .touchUpInside)
+            //            checkWeatherCellLabelView.addButton.addTarget(self, action: #selector(tapAddIndexButton), for: .touchUpInside)
         }
-        checkWeatherBasicNavigationView.locationButton.addTarget(self, action: #selector(tapLocationButton), for: .touchUpInside)
-        checkWeatherCellLabelView.addButton.addTarget(self, action: #selector(tapAddIndexButton), for: .touchUpInside)
     }
     
-    @objc func tapLocationButton() {
-        let locationSelectionView = LocationSelectionView()
-        self.navigationController?.pushViewController(locationSelectionView, animated: true)
+    private func configurePageViewController() {
+        pageViewController.view.snp.makeConstraints {
+            $0.top.equalTo(checkWeatherBasicNavigationView.snp.bottom)
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
     }
     
-    @objc func tapAddIndexButton() {
-        self.delegate?.sendFirstSequenceLocation(self.firstSequenceLocation)
-        self.present(self.addLivingIndexCellView, animated: true)
-    }
+    // 네비게이션 버튼
+    //    @objc func tapLocationButton() {
+    //        let locationSelectionView = LocationSelectionView()
+    //        self.navigationController?.pushViewController(locationSelectionView, animated: true)
+    //    }
+    
+    //    @objc func tapAddIndexButton() {
+    //        self.delegate?.sendFirstSequenceLocation(self.firstSequenceLocation)
+    //        self.present(self.addLivingIndexCellView, animated: true)
+    //    }
+    
+    
+    
 }
 
-extension CheckWeatherView {
-    func setLocationWeatherView() {
-        checkLocationWeatherView.locationLabel.configureLabel(text: "\(self.firstSequenceCity)", font: UIFont.KFont.appleSDNeoSemiBoldLarge, textColor: UIColor.KColor.white)
-        checkLocationWeatherView.temperatureLabel.configureLabel(text: "\(Int(weatherData.main[0].currentTemperature))°", font: UIFont.KFont.lexendExtraLarge, textColor: UIColor.KColor.white)
-        
-        checkLocationWeatherView.maxTemperatureLabel.configureLabel(text: "최고  \(Int(weatherData.main[0].dayMaxTemperature))°",font:  UIFont.KFont.appleSDNeoMediumSmall, textColor: UIColor.KColor.gray07)
-        checkLocationWeatherView.minTemperatureLabel.configureLabel(text: "최저  \(Int(weatherData.main[0].dayMinTemperature))°", font: UIFont.KFont.appleSDNeoMediumSmall, textColor: UIColor.KColor.gray07)
-        let currentWeatherNum = "currentWeather_\(weatherData.main[0].currentWeather)"
-        checkLocationWeatherView.weatherImage.image = UIImage(named: currentWeatherNum)
+extension CheckWeatherView: UIPageViewControllerDataSource, UIPageViewControllerDelegate {
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let index = dataViewControllers.firstIndex(of: viewController) else { return nil }
+        let previousIndex = index - 1
+        if previousIndex < 0 {
+            return nil
+        }
+        return dataViewControllers[previousIndex]
     }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let index = dataViewControllers.firstIndex(of: viewController) else { return nil }
+        let nextIndex = index + 1
+        if nextIndex == dataViewControllers.count {
+            return nil
+        }
+        return dataViewControllers[nextIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        
+        // set the pageControl.currentPage to the index of the current viewController in pages
+        if let viewControllers = pageViewController.viewControllers {
+            if let viewControllerIndex = self.dataViewControllers.firstIndex(of: viewControllers[0]) {
+                self.pageControl.currentPage = viewControllerIndex
+            }
+        }
+    }
+    
+    
 }
