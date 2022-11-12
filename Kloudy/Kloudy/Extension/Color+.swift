4551545063ae41d2246fd5049ccc167025cea20e
Byeonjinha <87685946+Byeonjinha@users.noreply.github.com>
2022-11-12 23:35:53 +0900

    💄 #53 : UIColor 수정 및 추가

--- Kloudy/Kloudy/Extension/Color+.swift
+++ Kloudy/Kloudy/Extension/Color+.swift
@@ -10,21 +10,17 @@ import UIKit
 extension UIColor {
     // 사용 방법 : UIColor.KColor.primaryDarkGreen
     enum KColor {
-        static var primaryDarkGreen: UIColor { UIColor(named: "PrimaryDarkGreen")! }
-        static var primaryGreen: UIColor { UIColor(named: "PrimaryGreen")! }
-        static var backgroundBlack: UIColor { UIColor(named: "BackgroundBlack")! }
-        static var black: UIColor { UIColor(named: "Black")! }
-        static var cellGray: UIColor { UIColor(named: "CellGray")! }
-        static var gray01: UIColor { UIColor(named: "Gray01")! }
-        static var gray02: UIColor { UIColor(named: "Gray02")! }
-        static var gray03: UIColor { UIColor(named: "Gray03")! }
-        static var gray04: UIColor { UIColor(named: "Gray04")! }
-        static var gray05: UIColor { UIColor(named: "Gray05")! }
-        static var gray06: UIColor { UIColor(named: "Gray06")! }
-        static var gray07: UIColor { UIColor(named: "Gray07")! }
-        static var white: UIColor { UIColor(named: "White")! }
-        static var red: UIColor { UIColor(named: "Red")! }
-        static var opacityOverlayBlack: UIColor { UIColor(named: "OpacityOverlayBlack")! }
+        static var black: UIColor { UIColor(named: "black")! }
+        static var primaryBlue01: UIColor { UIColor(named: "primaryBlue01")! }
+        static var primaryBlue02: UIColor { UIColor(named: "primaryBlue02")! }
+        static var primaryBlue03: UIColor { UIColor(named: "primaryBlue03")! }
+        static var primaryBlue04: UIColor { UIColor(named: "primaryBlue04")! }
+        static var primaryBlue05: UIColor { UIColor(named: "primaryBlue05")! }
+        static var primaryBlue06: UIColor { UIColor(named: "primaryBlue06")! }
+        static var gray01: UIColor { UIColor(named: "gray01")! }
+        static var gray02: UIColor { UIColor(named: "gray02")! }
+        static var gray03: UIColor { UIColor(named: "gray03")! }
+        static var white: UIColor { UIColor(named: "white")! }
         static var clear: UIColor { UIColor.clear }
     }
 }
