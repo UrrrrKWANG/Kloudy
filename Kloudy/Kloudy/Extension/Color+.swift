7b4e8a787e4b6d8a6f0f6fe9420336117e407b9a
Byeonjinha <87685946+Byeonjinha@users.noreply.github.com>
2022-11-17 20:18:33 +0900

    Style color asset 및 font 추가

--- Kloudy/Kloudy/Extension/Color+.swift
+++ Kloudy/Kloudy/Extension/Color+.swift
@@ -17,9 +17,14 @@ extension UIColor {
         static var primaryBlue04: UIColor { UIColor(named: "PrimaryBlue04")! }
         static var primaryBlue05: UIColor { UIColor(named: "PrimaryBlue05")! }
         static var primaryBlue06: UIColor { UIColor(named: "PrimaryBlue06")! }
+        static var primaryBlue07: UIColor { UIColor(named: "PrimaryBlue07")! }
         static var gray01: UIColor { UIColor(named: "Gray01")! }
         static var gray02: UIColor { UIColor(named: "Gray02")! }
         static var gray03: UIColor { UIColor(named: "Gray03")! }
+        static var gray04: UIColor { UIColor(named: "Gray04")! }
+        static var gray05: UIColor { UIColor(named: "Gray05")! }
+        static var indexYellow1: UIColor { UIColor(named: "IndexYellow1")! }
+        static var indexYellow2: UIColor { UIColor(named: "IndexYellow2")! }
         static var chartBlue: UIColor { UIColor(named: "ChartBlue")! }
         static var white: UIColor { UIColor(named: "White")! }
         static var clear: UIColor { UIColor.clear }
