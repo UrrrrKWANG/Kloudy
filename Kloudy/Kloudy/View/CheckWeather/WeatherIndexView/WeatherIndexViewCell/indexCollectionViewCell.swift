245c99bdf4c7cdbf99fc0be117198b7aeb03c0f4
Byeonjinha <87685946+Byeonjinha@users.noreply.github.com>
2022-11-30 00:06:36 +0900

    design #335 weatherIndexView 세부 디자인 수정

--- Kloudy/Kloudy/View/CheckWeather/WeatherIndexView/WeatherIndexViewCell/indexCollectionViewCell.swift
+++ Kloudy/Kloudy/View/CheckWeather/WeatherIndexView/WeatherIndexViewCell/indexCollectionViewCell.swift
@@ -13,6 +13,7 @@ class indexCollectionViewCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         self.layer.cornerRadius = 21
+        self.layer.applySketchShadow(color: UIColor.KColor.primaryBlue01, alpha: 0.15, x: 0, y: 0, blur: 10, spread: 0)
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
