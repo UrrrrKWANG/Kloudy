997dd0223fa96d23846b2d4baa1c75f39fc7a5b7
2youngjun <86882798+2youngjun@users.noreply.github.com>
2022-12-03 18:16:00 +0900

    Feat[#350] : 유저가 RequestAlert 가 선택헀을 때 다음 뷰로 이동

--- Kloudy/Kloudy/Application/OnboardingView.swift
+++ Kloudy/Kloudy/Application/OnboardingView.swift
@@ -12,25 +12,43 @@ import CoreLocation
 class OnboardingView: UIViewController {
     
     let nextButton = UIButton()
+    var timer: Timer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .cyan
+        layout()
+        attribute()
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(checkIsAlertDismissed), userInfo: nil, repeats: true)
+    }
+    
+    private func layout() {
         view.addSubview(nextButton)
         nextButton.snp.makeConstraints {
             $0.center.equalToSuperview()
             $0.size.equalTo(100)
         }
-        
+    }
+    
+    private func attribute() {
+        view.backgroundColor = UIColor.KColor.primaryBlue05
         nextButton.setTitle("다음", for: .normal)
-        nextButton.setTitleColor(.white, for: .normal)
+        nextButton.setTitleColor(UIColor.KColor.primaryBlue01, for: .normal)
         nextButton.addTarget(self, action: #selector(tapNextButton), for: .touchUpInside)
-        
     }
     
     @objc private func tapNextButton() {
         CLLocationManager().requestAlwaysAuthorization()
-        CLLocationManager().didCha
-        self.navigationController?.pushViewController(ViewController(), animated: true)
+    }
+    
+    @objc private func checkIsAlertDismissed() {
+        switch CLLocationManager().authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse, .restricted, .denied:
+            self.navigationController?.pushViewController(ViewController(), animated: true)
+            timer?.invalidate()
+        case .notDetermined:
+            return
+        @unknown default:
+            fatalError()
+        }
     }
 }
