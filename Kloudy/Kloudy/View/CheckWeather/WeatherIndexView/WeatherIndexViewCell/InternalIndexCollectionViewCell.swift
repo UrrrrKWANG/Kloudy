20ebf5618c693a9dae3fcffc984c2ca500e4df83
Byeonjinha <87685946+Byeonjinha@users.noreply.github.com>
2022-12-07 12:00:23 +0900

    feat #367 토스트 메시지 구현

--- Kloudy/Kloudy/View/CheckWeather/WeatherIndexView/WeatherIndexViewCell/InternalIndexCollectionViewCell.swift
+++ Kloudy/Kloudy/View/CheckWeather/WeatherIndexView/WeatherIndexViewCell/InternalIndexCollectionViewCell.swift
@@ -10,36 +10,83 @@ import RxSwift
 
 class InternalIndexCollectionViewCell: UICollectionViewCell {
     static let identifier = "InternalIndexCollectionViewCell"
-    
+    var borderWidth : CGFloat = 0 // Should be less or equal to the `radius` property
+    var radius : CGFloat = 10
+    var triangleHeight : CGFloat = 4
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    func showToast(message : String, font: UIFont = UIFont.KFont.appleSDNeoBold20, cntCorrect: Int) {
-        let toastLabel = UILabel(frame: CGRect(x: UIScreen.main.bounds.width/4, y: UIScreen.main.bounds.height - 170, width: 200, height: 40))
-        print("난 showTaost 야")
-        toastLabel.backgroundColor = UIColor.KColor.black
-        toastLabel.textColor = UIColor.black
-        toastLabel.font = font
-        toastLabel.textAlignment = .center;
-        toastLabel.text = message
-        //            toastLabel.alpha = 1.0
-        toastLabel.layer.cornerRadius = 10;
-        toastLabel.clipsToBounds = true
-        self.addSubview(toastLabel)
-        toastLabel.snp.makeConstraints{
-//            $0.top.leading.equalToSuperview().offset(30)
-//            $0.bottom.trailing.equalToSuperview().offset(100)
-            $0.edges.equalToSuperview().offset(10)
-            
+    func showToast(message: String, fontColor: UIColor, bgColor: UIColor) {
+        let toastView = ToastView( viewColor: bgColor,
+                                   tipStartX: message.count == 2 ? 40.5 : 52.5,
+                                   tipWidth: 12.0,
+                                   tipHeight: 8.0,
+                                   fontColor: fontColor,
+                                   text: message
+        )
+        self.addSubview(toastView)
+        toastView.snp.makeConstraints{
+            $0.top.equalTo(self.snp.bottom).offset(12)
+            $0.trailing.equalToSuperview()
         }
-        
-        UIView.animate(withDuration: 2.0, delay: 1.0, options: .curveEaseOut, animations: {
-            toastLabel.alpha = 0.0
+        UIView.animate(withDuration: 1.0, delay: 1.0, options: .curveEaseOut, animations: {
+            toastView.alpha = 0
         }, completion: {(isCompleted) in
-            toastLabel.removeFromSuperview()
+            toastView.removeFromSuperview()
         })
+    }
+}
+
+class ToastView: UIView {
+    init(
+        viewColor: UIColor,
+        tipStartX: CGFloat,
+        tipWidth: CGFloat,
+        tipHeight: CGFloat,
+        fontColor: UIColor,
+        text: String
+    ) {
+        super.init(frame: .zero)
+        self.backgroundColor = viewColor
+        
+        let path = CGMutablePath()
+        let tipWidthCenter = tipWidth / 2.0
+        let endXWidth = tipStartX + tipWidth
+        
+        path.move(to: CGPoint(x: tipStartX, y: 0))
+        path.addLine(to: CGPoint(x: tipStartX + tipWidthCenter, y: -tipHeight))
+        path.addLine(to: CGPoint(x: endXWidth, y: 0))
+        path.addLine(to: CGPoint(x: 0, y: 0))
+        
+        let shape = CAShapeLayer()
+        shape.path = path
+        shape.fillColor = viewColor.cgColor
+        
+        self.layer.insertSublayer(shape, at: 0)
+        self.layer.masksToBounds = false
+        self.layer.cornerRadius = 6
+        addLabel(fontColor: fontColor, text: text)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    private func addLabel(fontColor: UIColor, text: String) {
+        let toastLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 0, height: 0))
+        toastLabel.font = UIFont.KFont.appleSDNeoMedium15
+        toastLabel.textAlignment = .center;
+        toastLabel.textColor = fontColor
+        toastLabel.text = text
+        toastLabel.layer.cornerRadius = 6;
+        toastLabel.clipsToBounds = true
+        
+        self.addSubview(toastLabel)
+        toastLabel.snp.makeConstraints {
+            $0.top.bottom.equalToSuperview().inset(6)
+            $0.left.right.equalToSuperview().inset(16)
+        }
     }
 }
